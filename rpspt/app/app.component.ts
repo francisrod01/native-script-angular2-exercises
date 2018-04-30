08c680e8fa9bee9d5e98c50df9d41af3b39a2ff1
Francis Rodrigues <francis.rod01@outlook.com>
2018-04-30 13:29:20 -0300

    [rpspt] Code organization - Global setup

--- rpspt/app/app.component.ts
+++ rpspt/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "ns-app",
-    templateUrl: "app.component.html",
+    // templateUrl: "app.component.html",
+    template: `
+        <Label text="My App" textWrap="true"></Label>
+        
+    `
 })
 
 export class AppComponent { }
